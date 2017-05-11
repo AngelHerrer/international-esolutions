93b7921892078d6f22c9726a05dc41e80334cf99
Angel Herrera <enyel26435@live.com.mx>
2017-05-10 22:08:22 -0500

    a

--- pages/executeSaveEmp.php
+++ pages/executeSaveEmp.php
@@ -1,12 +1,22 @@
-<?php 
+<?php
 error_reporting(E_ALL ^ E_NOTICE);
 include 'function.php';
 $valores = new Valores();
 extract($_REQUEST);
-echo $claveCbb.'<br>'.$descripcion.'<br>'.$marca.'<br>'.$empresa.'<br>'.$precio.'<br>'.$preciou.'<br>'.$preciov.'<br>'.$grcn.'<br>'.$presentacion.'<br>'.$porigen.'<br>'.$fechve.'<br>'.$obse.'<br>'.$rsani.'<br>'.$catft;
+echo $claveCbb . '<br>' . $descripcion . '<br>' . $marca . '<br>' . $empresa . '<br>' . $precio . '<br>' . $preciou . '<br>' . $preciov . '<br>' . $grcn . '<br>' . $presentacion . '<br>' . $porigen . '<br>' . $fechve . '<br>' . $obse . '<br>' . $rsani . '<br>' . $catft;
+
+$fichas = './fichas/'.$claveCbb.'/';
+$sanitarios = './sanitario/'.$claveCbb.'/';
+
+mkdir($fichas, 0777, true);
+
+mkdir($sanitarios, 0777, true);
+
+file_put_contents($rsani, $fichas);
+
+file_put_contents($catft, $sanitarios);
 die();
-$valores->saveEmployee($claveCbb, $descripcion, $marca, $empresa, $precio, $preciou,$preciov,$grcn,$presentacion,$porigen,$fechve,$obse,$rsani,$catft);
-  
+$valores->saveEmployee($claveCbb, $descripcion, $marca, $empresa, $precio, $preciou, $preciov, $grcn, $presentacion, $porigen, $fechve, $obse, $rsani, $catft);
 ?>
 
 
