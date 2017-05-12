8f68d8f709a4ba48cdd9911c13efa4460181b750
Angel Herrera <enyel26435@live.com.mx>
2017-05-11 19:35:32 -0500

    get form

--- pages/function.php
+++ pages/function.php
@@ -29,21 +29,22 @@ class Valores {
 
     function saveEmployee($claveCbb, $descripcion, $marca, $empresa, $precio, $preciou, $preciov, $grcn, $presentacion, $porigen, $fechve, $obse, $rsani, $catft) {
         echo $claveCbb . '<br>' . $descripcion . '<br>' . $marca . '<br>' . $empresa . '<br>' . $precio . '<br>' . $preciou . '<br>' . $preciov . '<br>' . $grcn . '<br>' . $presentacion . '<br>' . $porigen . '<br>' . $fechve . '<br>' . $obse . '<br>' . $rsani . '<br>' . $catft;
-        
+
         include 'conexion.php';
         $query = "INSERT INTO material VALUES ('$claveCbb','$descripcion','$marca',$empresa,$precio,$preciou,$preciov,'$grcn','$presentacion',$porigen,'$fechve','$obse','$rsani','$catft')";
         echo $query;
         if (!$result = mysqli_query($liga, $query)) {
-//            echo '<script type="text/javascript">';
-//            echo 'window.location.href="addEmp.php?succes=ok";';
-//            echo '</script>';
-              echo("Error description: " . mysqli_error($liga));
+            echo '<script type="text/javascript">';
+            echo 'window.location.href="addEmp.php?succes=ok";';
+            echo '</script>';
+        } else {
+            echo("Error description: " . mysqli_error($liga));
         }
     }
-    
-    function desc_mat ($id_mat){
+
+    function desc_mat($id_mat) {
         include 'conexion.php';
-        $query = "select * from material where id_mat=".$id_mat;
+        $query = "select * from material where id_mat=" . $id_mat;
         $result = mysqli_query($liga, $query);
         while ($row = mysqli_fetch_array($result)) {
             $claveCbb = $row['id_mat'];
@@ -60,31 +61,29 @@ class Valores {
             $obse = $row['obse_mat'];
             $rsani = $row['rsani_mat'];
             $catft = $row['catft_mat'];
-            
+
 //            echo '<table><tr><td>' . $claveCbb . '</td><td>' . $descripcion . '</td><td>' . $marca . '</td><td>' . $empresa . '</td><td>' . $presentacion . '</td><td>' . $precio . '</td></tr></table>';
             //echo $claveCbb.'<br>'.$descripcion.'<br>'.$marca.'<br>'.$empresa.'<br>'.$precio.'<br>'.$preciou.'<br>'.$preciov.'<br>'.$grcn.'<br>'.$presentacion.'<br>'.$porigen.'<br>'.$fechve.'<br>'.$obse.'<br>'.$rsani.'<br>'.$catft;
-            return array($claveCbb,$descripcion,$marca,$empresa,$precio,$preciou,$preciov,$grcn,$presentacion,$porigen,$fechve,$obse,$rsani,$catft);
+            return array($claveCbb, $descripcion, $marca, $empresa, $precio, $preciou, $preciov, $grcn, $presentacion, $porigen, $fechve, $obse, $rsani, $catft);
         }
     }
 
-    
-    function saveCompany($nameComp, $telComp, $mailComp, $rfcComp, $dirComp, $cuentComp, $bancoComp, $contactComp){
+    function saveCompany($nameComp, $telComp, $mailComp, $rfcComp, $dirComp, $cuentComp, $bancoComp, $contactComp) {
         include 'conexion.php';
         $query = "INSERT INTO empresas (name_empre,tel_empre,mail_empre,rfc_empre,dir_empre,cuenta_empre,banco_empre,contact_empre) VALUES ('$nameComp','$telComp','$mailComp','$rfcComp','$dirComp','$cuentComp','$bancoComp','$contactComp')";
-        
-        if($result = mysqli_query($liga, $query)){
-          echo '<script type="text/javascript">';
-          echo 'window.location.href="addComp.php?succes=ok";';
-          echo '</script>';
-        }else{
-          echo '<script type="text/javascript">';
-          echo 'window.location.href="addComp.php?succes=false";';
-          echo '</script>';  
+
+        if ($result = mysqli_query($liga, $query)) {
+            echo '<script type="text/javascript">';
+            echo 'window.location.href="addComp.php?succes=ok";';
+            echo '</script>';
+        } else {
+            echo '<script type="text/javascript">';
+            echo 'window.location.href="addComp.php?succes=false";';
+            echo '</script>';
         }
-        
     }
-    
-    function getCompanyAll(){
+
+    function getCompanyAll() {
         include 'conexion.php';
         $query = "select * from empresas";
         $result = mysqli_query($liga, $query);
@@ -99,14 +98,14 @@ class Valores {
             $contactCompany = $row['contact_empre'];
             $idCompany = $row['id_empresas'];
 
-            echo '<tr><td>' . $nameCompany . '</td><td>' . $telCompany . '</td><td>' . $mailCompany . '</td><td>' . $rfcCompany . '</td><td>' . $dirCompany . '</td><td>' . $cuentaCompany . '</td><td>'.$bancoCompany.'</td><td>'.$contactCompany.'</td><td><a href="editCompany.php?id_comp='.$idCompany.'">'.Editar.'</a></td></tr>';
+            echo '<tr><td>' . $nameCompany . '</td><td>' . $telCompany . '</td><td>' . $mailCompany . '</td><td>' . $rfcCompany . '</td><td>' . $dirCompany . '</td><td>' . $cuentaCompany . '</td><td>' . $bancoCompany . '</td><td>' . $contactCompany . '</td><td><a href="editCompany.php?id_comp=' . $idCompany . '">' . Editar . '</a></td></tr>';
             //<td align="center"><a href="getEmpleado.php?idEmp=' . $idEmp . '"><span style="margin-left:10px;" class="glyphicon glyphicon-eye-open"></span></a></td>
         }
     }
-    
-    function getCompany($id_comp){
+
+    function getCompany($id_comp) {
         include 'conexion.php';
-        $query = 'select * from empresas where id_empresas='.$id_comp;
+        $query = 'select * from empresas where id_empresas=' . $id_comp;
         $result = mysqli_query($liga, $query);
         while ($row = mysqli_fetch_array($result)) {
             $nameCompany = $row['name_empre'];
@@ -118,25 +117,24 @@ class Valores {
             $bancoCompany = $row['banco_empre'];
             $contactCompany = $row['contact_empre'];
             $idCompany = $row['id_empresas'];
-            
+
 
             //echo $nameCompany.'<br>'.$telCompany.'<br>'.$mailCompany.'<br>'.$rfcCompany.'<br>'.$dirCompany.'<br>'.$cuentaCompany.'<br>'.$bancoCompany.'<br>'.$contactCompany;
-            return array($nameCompany,$telCompany,$mailCompany,$rfcCompany,$dirCompany,$cuentaCompany,$bancoCompany,$contactCompany,$idCompany);
+            return array($nameCompany, $telCompany, $mailCompany, $rfcCompany, $dirCompany, $cuentaCompany, $bancoCompany, $contactCompany, $idCompany);
         }
     }
-    
-        function updateCompany($telCompany,$mailCompany,$rfcCompany,$dirCompany,$cuentaCompany,$bancoCompany,$contactCompany,$idCompany) {
+
+    function updateCompany($telCompany, $mailCompany, $rfcCompany, $dirCompany, $cuentaCompany, $bancoCompany, $contactCompany, $idCompany) {
         include 'conexion.php';
         $query = "UPDATE `empresas` SET `tel_empre`='$telCompany',`mail_empre`='$mailCompany',"
                 . "`rfc_empre`='$rfcCompany',`dir_empre`='$dirCompany',`cuenta_empre`='$cuentaCompany',`banco_empre`='$bancoCompany',"
                 . "`contact_empre`='$contactCompany' WHERE `id_empresas`=$idCompany";
 
-       
+
         $result = mysqli_query($liga, $query) or die("<script>window.location.href='index.php?edit=no';</script>");
-        
     }
-    
-    function getCompanySelect(){
+
+    function getCompanySelect() {
         include 'conexion.php';
         $query = "select * from empresas";
         $result = mysqli_query($liga, $query);
@@ -144,18 +142,18 @@ class Valores {
             $nameCompany = $row['name_empre'];
             $idCompany = $row['id_empresas'];
 
-            echo '<option value="'.$idCompany.'">'.$nameCompany.'</option>';
+            echo '<option value="' . $idCompany . '">' . $nameCompany . '</option>';
             //<td align="center"><a href="getEmpleado.php?idEmp=' . $idEmp . '"><span style="margin-left:10px;" class="glyphicon glyphicon-eye-open"></span></a></td>
         }
     }
-    
-    function updateMaterial($claveCbb,$descripcion,$marca,$empresa,$precio,$preciou,$preciov,$grcn,$presentacion,$porigen,$fechve,$obse) {
+
+    function updateMaterial($claveCbb, $descripcion, $marca, $empresa, $precio, $preciou, $preciov, $grcn, $presentacion, $porigen, $fechve, $obse) {
         include 'conexion.php';
         $query = "UPDATE `material` SET `marc_mat`='$marca',`id_empr`='$empresa',"
                 . "`prcio_mat`='$precio',`prciou_mat`='$preciou',`prciov_mat`='$preciov',`grcn_mat`='$grcn',"
                 . "`prese_mat`='$presentacion',`id_pais`='$porigen',`fechve_mat`='$fechve',`obse_mat`='$obse' WHERE `id_mat`=$claveCbb";
 
-       
+
         $result = mysqli_query($liga, $query) or die();
         //"<script>window.location.href='index.php?edit=no';</script>"
     }
